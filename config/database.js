91b80468d6b9c29b12fad509e59f870fe94fb0e0
prateek <prateekjangir4576@gmail.com>
2025-08-21 17:37:57 +0530

    Update database config for Render deployment

--- config/database.js
+++ config/database.js
@@ -11,6 +11,7 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false } // required by Render
 });
 
 export default pool;
