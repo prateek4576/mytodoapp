1942849185003ce6c6512a63af8cfee3123e5bec
prateek <prateekjangir4576@gmail.com>
2025-08-21 19:47:55 +0530

    Added SSL config for a Render PostgreSQL

--- config/database.js
+++ config/database.js
@@ -6,10 +6,12 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false   // This tells Node.js to accept Render's SSL certificate
-  }
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    // ssl: { rejectUnauthorized: false } 
 });
 
 export default pool;
