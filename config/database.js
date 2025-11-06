3a52d90a542b2971691fc0daef4e60d393af3517
prateek <prateekjangir4576@gmail.com>
2025-11-06 15:58:57 +0530

    Fixed SSL issue for Render database

--- config/database.js
+++ config/database.js
@@ -5,13 +5,15 @@ dotenv.config();
 
 const { Pool } = pkg;
 
+const isRender = process.env.RENDER === 'true';
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false } 
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: isRender ? false : { rejectUnauthorized: false }
 });
 
 export default pool;
